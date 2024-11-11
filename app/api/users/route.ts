2186c76357ba1ac50b6eddc747b47f59fa366464
AlvaroAndres123 <alvaroandrescasco1@gmail.com>
2024-11-10 20:12:44 -0600

    Route fix for vercel deployment

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,18 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@vercel/postgres';
 import { Buffer } from 'buffer';
 
-export async function handler(req: NextRequest) {
-  switch (req.method) {
-    case 'GET':
-      return handleGet(req);
-    case 'PATCH':
-      return handlePatch(req);
-    default:
-      return NextResponse.json({ error: 'Método no permitido' }, { status: 405 });
-  }
-}
 
-async function handleGet(req: NextRequest) {
+export async function GET(req: NextRequest) {
   let client;
   try {
     client = await db.connect();
@@ -26,13 +16,12 @@ async function handleGet(req: NextRequest) {
   }
 }
 
-
-
-async function handlePatch(req: NextRequest) {
+export async function PATCH(req: NextRequest) {
   const { id, name, user_image } = await req.json();
   let client;
   try {
     client = await db.connect();
+
 
     const imageBuffer = Buffer.from(user_image.split(',')[1], 'base64');
 
@@ -48,6 +37,3 @@ async function handlePatch(req: NextRequest) {
     return NextResponse.json({ error: 'Error updating user' }, { status: 500 });
   }
 }
-
-
-export { handler as GET, handler as PATCH };
